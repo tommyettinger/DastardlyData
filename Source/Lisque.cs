aa4c1c630aeb0b7fcb1394b6fe377cc7a58f5528
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-11 20:47:01 -0700

    Add()/PushLast(), and necessary Resize().
    These ones are fairly straightforward copies of my jdkgdxds ObjectDeque code.

--- Source/Lisque.cs
+++ Source/Lisque.cs
@@ -10,7 +10,7 @@ public class Lisque<T> : ILisque<T>
     protected int Size;
     protected int Head;
     protected int Tail;
-    
+
     internal int Version;
 
     public Lisque(int capacity = DefaultCapacity)
@@ -34,7 +34,7 @@ public class Lisque<T> : ILisque<T>
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        PushLast(item);
     }
 
     public void Clear()
@@ -59,6 +59,7 @@ public class Lisque<T> : ILisque<T>
 
     public int Count => Size;
     public bool IsReadOnly => false;
+
     public int IndexOf(T item)
     {
         throw new NotImplementedException();
@@ -87,7 +88,16 @@ public class Lisque<T> : ILisque<T>
 
     public void PushLast(T item)
     {
-        throw new NotImplementedException();
+        if (Size == Items.Length)
+        {
+            Resize(Items.Length << 1);
+        }
+
+        if (++Size == 1) Tail = Head;
+        else if (++Tail == Items.Length) Tail = 0;
+        Items[Tail] = item;
+        Version++;
+
     }
 
     public T PopFirst()
@@ -110,7 +120,7 @@ public class Lisque<T> : ILisque<T>
         get => Size == 0 ? throw new InvalidOperationException("Lisque is empty.") : Items[Head];
         set
         {
-            if(Size == 0) throw new InvalidOperationException("Lisque is empty.");
+            if (Size == 0) throw new InvalidOperationException("Lisque is empty.");
             Items[Head] = value;
         }
     }
@@ -120,9 +130,49 @@ public class Lisque<T> : ILisque<T>
         get => Size == 0 ? throw new InvalidOperationException("Lisque is empty.") : Items[Tail];
         set
         {
-            if(Size == 0) throw new InvalidOperationException("Lisque is empty.");
+            if (Size == 0) throw new InvalidOperationException("Lisque is empty.");
             Items[Tail] = value;
         }
+    }
 
+    /// <summary>
+    /// Resizes the backing array. 
+    /// </summary>
+    /// <remarks>
+    /// The newSize should be greater than the current size; otherwise, newSize will be set to
+    /// size and the resize to the same size will (for most purposes) be wasted effort. If this is not empty, this will
+    /// rearrange the items internally to be linear and have the head at index 0, with the tail at {@code size - 1}.
+    /// This always allocates a new internal backing array.
+    /// </remarks>
+    /// <param name="newSize">The new capacity for the backing array.</param>
+    protected void Resize(int newSize)
+    {
+        if (newSize < Size)
+            newSize = Size;
+        var head = Head;
+        var tail = Tail;
+
+        var newArray = new T[Math.Max(1, newSize)];
+
+        if (Size > 0)
+        {
+            if (head <= tail)
+            {
+                // Continuous
+                Array.Copy(Items, head, newArray, 0, tail - head + 1);
+            }
+            else
+            {
+                // Wrapped
+                var rest = Items.Length - head;
+                Array.Copy(Items, head, newArray, 0, rest);
+                Array.Copy(Items, 0, newArray, rest, tail + 1);
+            }
+
+            Head = 0;
+            Tail = Size - 1;
+        }
+
+        Items = newArray;
     }
 }
