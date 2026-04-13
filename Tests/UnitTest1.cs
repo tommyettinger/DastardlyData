08ec304ff92470a5833d047f32896835cf4aa123
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-12 19:02:37 -0700

    Simple test seems to work.
    More involved tests will come in as we can use them.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Dastardly.Data;
+
 namespace Tests;
 
 public class Tests {
@@ -8,7 +10,14 @@ public class Tests {
     }
 
     [Test]
-    public void Test1() {
-        Assert.Pass();
+    public void TestPushAndGet()
+    {
+        Lisque<string> lisque = new(16);
+        lisque.PushLast("beta");
+        lisque.PushFirst("alpha");
+        lisque.PushLast("gamma");
+        Assert.That(lisque[0], Is.EqualTo("alpha"));
+        Assert.That(lisque[1], Is.EqualTo("beta"));
+        Assert.That(lisque[2], Is.EqualTo("gamma"));
     }
 }
