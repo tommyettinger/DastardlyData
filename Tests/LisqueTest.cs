88127d275acc0467b64e49718d8638f01593fbbd
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-12 20:03:23 -0700

    The head wraps when inserted at the start.

---
+++ Tests/LisqueTest.cs
@@ -0,0 +1,38 @@
+using Dastardly.Data;
+
+namespace Tests;
+
+public class Tests {
+    
+    [SetUp]
+    public void Setup() {
+
+    }
+
+    [Test]
+    public void TestPushAndGet()
+    {
+        Lisque<string> lisque = new(16);
+        lisque.PushLast("beta");
+        lisque.PushFirst("alpha");
+        lisque.PushLast("gamma");
+        Assert.That(lisque[0], Is.EqualTo("alpha"));
+        Assert.That(lisque[1], Is.EqualTo("beta"));
+        Assert.That(lisque[2], Is.EqualTo("gamma"));
+    }
+
+    [Test]
+    public void TestHeadWraps()
+    {
+        Lisque<string> lisque = new(4);
+        lisque.PushFirst("gamma");
+        lisque.PushFirst("beta");
+        lisque.PushFirst("alpha");
+        Assert.That(lisque[0], Is.EqualTo("alpha"));
+        Assert.That(lisque[1], Is.EqualTo("beta"));
+        Assert.That(lisque[2], Is.EqualTo("gamma"));
+        Assert.That(lisque.IndexOf("alpha"), Is.EqualTo(0));
+        Assert.That(lisque.IndexOf("beta"), Is.EqualTo(1));
+        Assert.That(lisque.IndexOf("gamma"), Is.EqualTo(2));
+    }
+}
