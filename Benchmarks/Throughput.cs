1188a36e6830f304fde28510fac557f0b76413f3
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-13 21:14:33 -0700

    OK... Maybe avoid side effects so much?

--- Benchmarks/Throughput.cs
+++ Benchmarks/Throughput.cs
@@ -9,27 +9,30 @@ namespace Benchmarks {
 
         private Lisque<int>? _lisque;
 
-        [GlobalSetup(Target = nameof(LisqueAdd))]
+        [IterationSetup(Target = nameof(LisqueAdd))]
         public void LisqueAddSetup()
         {
-            if(_lisque == null) 
-                _lisque = new(16);
-            else if(_lisque.Count >= 10000)
-                _lisque.Clear();
+            _lisque = new(16);
         }
-
+        [IterationCleanup(Target = nameof(LisqueAdd))]
+        public void LisqueAddCleanup()
+        {
+            _lisque!.Clear();
+        }
         [Benchmark]
         public void LisqueAdd() {
             _lisque!.Add(1);
         }
 
-        [GlobalSetup(Target = nameof(LisqueInsertStart))]
+        [IterationSetup(Target = nameof(LisqueInsertStart))]
         public void LisqueInsertStartSetup()
         {
-            if(_lisque == null) 
-                _lisque = new(16);
-            else if(_lisque.Count >= 10000)
-                _lisque.Clear();
+            _lisque = new(16);
+        }
+        [IterationCleanup(Target = nameof(LisqueInsertStart))]
+        public void LisqueInsertStartCleanup()
+        {
+            _lisque!.Clear();
         }
         [Benchmark]
         public void LisqueInsertStart() {
@@ -38,27 +41,30 @@ namespace Benchmarks {
 
         private List<int>? _list;
 
-        [GlobalSetup(Target = nameof(ListAdd))]
+        [IterationSetup(Target = nameof(ListAdd))]
         public void ListAddSetup()
         {
-            if(_list == null) 
-                _list = new(16);
-            else if(_list.Count >= 10000)
-                _list.Clear();
+            _list = new(16);
         }
-
+        [IterationCleanup(Target = nameof(ListAdd))]
+        public void ListAddCleanup()
+        {
+            _list!.Clear();
+        }
         [Benchmark]
         public void ListAdd() {
             _list!.Add(1);
         }
 
-        [GlobalSetup(Target = nameof(ListInsertStart))]
+        [IterationSetup(Target = nameof(ListInsertStart))]
         public void ListInsertStartSetup()
         {
-            if(_list == null) 
-                _list = new(16);
-            else if(_list.Count >= 10000)
-                _list.Clear();
+            _list = new(16);
+        }
+        [IterationCleanup(Target = nameof(ListInsertStart))]
+        public void ListInsertStartCleanup()
+        {
+            _list!.Clear();
         }
         [Benchmark]
         public void ListInsertStart() {
@@ -67,28 +73,31 @@ namespace Benchmarks {
 
         private LinkedList<int>? _linkedList;
 
-        [GlobalSetup(Target = nameof(LinkedListAdd))]
+        [IterationSetup(Target = nameof(LinkedListAdd))]
         public void LinkedListAddSetup()
         {
-            if(_linkedList == null) 
-                _linkedList = new();
-            else if(_linkedList.Count >= 10000)
-                _linkedList.Clear();
+            _linkedList = new();
+        }
+        [IterationCleanup(Target = nameof(LinkedListAdd))]
+        public void LinkedListAddCleanup()
+        {
+            _linkedList!.Clear();
         }
         [Benchmark]
         public void LinkedListAdd() {
             _linkedList!.AddLast(1);
         }
 
-        [GlobalSetup(Target = nameof(LinkedListInsertStart))]
+        [IterationSetup(Target = nameof(LinkedListInsertStart))]
         public void LinkedListInsertStartSetup()
         {
-            if(_linkedList == null) 
-                _linkedList = new();
-            else if(_linkedList.Count >= 10000)
-                _linkedList.Clear();
+            _linkedList = new();
         }
-
+        [IterationCleanup(Target = nameof(LinkedListInsertStart))]
+        public void LinkedListInsertStartCleanup()
+        {
+            _linkedList!.Clear();
+        }
         [Benchmark]
         public void LinkedListInsertStart() {
             _linkedList!.AddFirst(1);
