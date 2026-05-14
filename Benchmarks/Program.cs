ddb7aaf81d75f28dee8cd93c1d480c87bf1ecd5c
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-13 20:29:26 -0700

    We're going to use Serilog for benchmarks.
    Not sure specifically why I picked this one, but they all should work.

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,28 +1,30 @@
 ﻿using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
+using Serilog;
 
 namespace Benchmarks;
 
 public class Program {
     static void Main(string[] args) {
-        /*
-        // Configure how you want your benchmarks to output additional data, Serilog, Savage.Logs, and ZLogger are all good ways to do this
-        // Savage.Logs
-        var loggySettings = LogPipelineSettings.Default();
-        loggyLogger = new LogPipeline().Initialize(loggySettings);
+         /*
+         // Configure how you want your benchmarks to output additional data, Serilog, Savage.Logs, and ZLogger are all good ways to do this
+         // Savage.Logs
+         var loggySettings = LogPipelineSettings.Default();
+         loggyLogger = new LogPipeline().Initialize(loggySettings);
+*/
+          // Serilog
+          Serilog.Log.Logger = new LoggerConfiguration()
+                                     .WriteTo.Console()
+                                     .CreateLogger();
+          /*
+         // ZLogger
+         using var factory = LoggerFactory.Create(logging => {
+             logging.SetMinimumLevel(LogLevel.Trace);
 
-         // Serilog
-         Serilog.Log.Logger = new LoggerConfiguration()
-                                    .WriteTo.Console()
-                                    .CreateLogger();
-        // ZLogger
-        using var factory = LoggerFactory.Create(logging => {
-            logging.SetMinimumLevel(LogLevel.Trace);
-
-            // Add ZLogger provider to ILoggingBuilder
-            logging.AddZLoggerConsole();
-        });
-        */
+             // Add ZLogger provider to ILoggingBuilder
+             logging.AddZLoggerConsole();
+         });
+         */
 
         Summary? summary;
         if (TryParseArguments(args, out Mode runMode, out string argumentErrorMessage) is false) Console.WriteLine(argumentErrorMessage);
