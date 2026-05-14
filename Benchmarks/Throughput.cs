b3943fe72e502ac7f6c744347e9529f45cb60983
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-13 21:06:19 -0700

    The O(n) inserts were going to take FOREVER.
    I set it to Clear() the IList if Count is 5 digits or more. The code for LisqueInsertStart() was also wrong!

--- Benchmarks/Throughput.cs
+++ Benchmarks/Throughput.cs
@@ -7,52 +7,91 @@ namespace Benchmarks {
     public class Throughput
     {
 
-        private Lisque<int> _lisque = null!;
+        private Lisque<int>? _lisque;
 
         [GlobalSetup(Target = nameof(LisqueAdd))]
-        public void LisqueAddSetup() => _lisque = new(16);
+        public void LisqueAddSetup()
+        {
+            if(_lisque == null) 
+                _lisque = new(16);
+            else if(_lisque.Count >= 10000)
+                _lisque.Clear();
+        }
+
         [Benchmark]
         public void LisqueAdd() {
-            _lisque.Add(1);
+            _lisque!.Add(1);
         }
 
         [GlobalSetup(Target = nameof(LisqueInsertStart))]
-        public void LisqueInsertStartSetup() => _list = new(16);
+        public void LisqueInsertStartSetup()
+        {
+            if(_lisque == null) 
+                _lisque = new(16);
+            else if(_lisque.Count >= 10000)
+                _lisque.Clear();
+        }
         [Benchmark]
         public void LisqueInsertStart() {
-            _list.Insert(0, 1);
+            _lisque!.Insert(0, 1);
         }
 
-        private List<int> _list = null!;
+        private List<int>? _list;
 
         [GlobalSetup(Target = nameof(ListAdd))]
-        public void ListAddSetup() => _list = new(16);
+        public void ListAddSetup()
+        {
+            if(_list == null) 
+                _list = new(16);
+            else if(_list.Count >= 10000)
+                _list.Clear();
+        }
+
         [Benchmark]
         public void ListAdd() {
-            _list.Add(1);
+            _list!.Add(1);
         }
 
         [GlobalSetup(Target = nameof(ListInsertStart))]
-        public void ListInsertStartSetup() => _list = new(16);
+        public void ListInsertStartSetup()
+        {
+            if(_list == null) 
+                _list = new(16);
+            else if(_list.Count >= 10000)
+                _list.Clear();
+        }
         [Benchmark]
         public void ListInsertStart() {
-            _list.Insert(0, 1);
+            _list!.Insert(0, 1);
         }
 
-        private LinkedList<int> _linkedList = null!;
+        private LinkedList<int>? _linkedList;
 
         [GlobalSetup(Target = nameof(LinkedListAdd))]
-        public void LinkedListAddSetup() => _linkedList = new();
+        public void LinkedListAddSetup()
+        {
+            if(_linkedList == null) 
+                _linkedList = new();
+            else if(_linkedList.Count >= 10000)
+                _linkedList.Clear();
+        }
         [Benchmark]
         public void LinkedListAdd() {
-            _linkedList.AddLast(1);
+            _linkedList!.AddLast(1);
         }
 
         [GlobalSetup(Target = nameof(LinkedListInsertStart))]
-        public void LinkedListInsertStartSetup() => _linkedList = new();
+        public void LinkedListInsertStartSetup()
+        {
+            if(_linkedList == null) 
+                _linkedList = new();
+            else if(_linkedList.Count >= 10000)
+                _linkedList.Clear();
+        }
+
         [Benchmark]
         public void LinkedListInsertStart() {
-            _linkedList.AddFirst(1);
+            _linkedList!.AddFirst(1);
         }
     }
 }
